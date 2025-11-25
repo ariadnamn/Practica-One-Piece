2484e90588ce960a0803c8b1c5d7e6fe2e4607b5
Diego <diego.e.aguilar.ojeda@gmail.com>
2025-11-25 16:13:28 +0100

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,2 +1,4 @@
-titulo = "=" * 30 + "{}" + "=" * 30
-print(titulo.format(" Menu 0 (One Piece) "))
+titulo = "=" * 20 + "{}" + "=" * 20+"\n"
+menu0="\n1)Play\n2)Create\n3)Edit\n4)List\n5)Exit"
+print(titulo.format(" Menu 0 (One Piece) "), menu0)
+op_menu0=int(input("\n-> Option: "))
